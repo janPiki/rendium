bb85df44ba9192d53287218a213cf59868d08c78
janPiki <janpiki273@proton.me>
2025-08-09 12:31:19 +0700

    Changed shader name from shader.wgsl to default.wgsl

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,7 @@ impl State {
 
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+            source: wgpu::ShaderSource::Wgsl(include_str!("default.wgsl").into()),
         });
 
         let render_pipeline_layout =
