4a4d447757b1a20869bc18324f0ea8f5a28df71f
janPiki <janpiki273@proton.me>
2025-08-24 07:26:08 +0700

    I though git add . adds every file. I guess not

--- examples/shapes.rs
+++ examples/shapes.rs
@@ -9,7 +9,12 @@ fn main() {
             rd.draw(Color::BLACK, |d| {
                 d.draw_rect((250.0, 250.0).into(), 200, 200, Color::BLUE);
                 d.draw_circle((100.0, 100.0).into(), 100, Color::WHITE, 20);
-                d.draw_line((100.0, 600.0).into(), (600.0, 600.0).into(), 10, Color::RED);
+                d.draw_line(
+                    (100.0, 600.0).into(),
+                    (600.0, 600.0).into(),
+                    10,
+                    Color(255, 0, 0, 100),
+                );
             });
         });
 }
--- examples/triangle.rs
+++ examples/triangle.rs
@@ -6,9 +6,9 @@ fn main() {
         .with_title("Example 1")
         .run(|rd| {
             rd.draw(Color::BLACK, |d| {
-                d.add_vertex([350.0, 200.0, 0.0], Color::RED);
-                d.add_vertex([150.0, 500.0, 0.0], Color::GREEN);
-                d.add_vertex([550.0, 500.0, 0.0], Color::BLUE);
+                d.add_vertex([350.0, 200.0, 0.0], Color::RED, [0.0, 0.0], 0);
+                d.add_vertex([150.0, 500.0, 0.0], Color::GREEN, [0.0, 0.0], 0);
+                d.add_vertex([550.0, 500.0, 0.0], Color::BLUE, [0.0, 0.0], 0);
 
                 d.add_index(0);
                 d.add_index(1);
